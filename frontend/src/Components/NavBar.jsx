acf31d022a60144a1f57fccfddeb185c4ed82dde
Paul <mwapaul265@gmail.com>
2024-08-23 01:16:09 +0300

    first commit

---
+++ frontend/src/Components/NavBar.jsx
@@ -0,0 +1,12 @@
+import {assets} from "../assets/assets"
+const NavBar = () => {
+  return (
+    <div className="flex items-center justify-between py-5 font-medium">
+        <img src={assets.logo} alt="" className="" />
+        <ul ></ul>
+      
+    </div>
+  )
+}
+
+export default NavBar
